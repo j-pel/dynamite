c371caf7050d068e73caf91f917cb64f0e3eed50
Jorge M. Peláez <jmpelaez@gmail.com>
2016-04-09 00:26:35 -0500

    Create dynamite.ex

---
+++ lib/dynamite.ex
@@ -0,0 +1,33 @@
+defmodule Dynamite do
+  use Application
+
+  def set_site(site) do
+    Application.put_env(:dynamite,:site,site)
+    compile_routes
+  end
+  
+  def compile_routes do
+    site = Application.fetch_env!(:dynamite, :site)
+    sites = Application.fetch_env!(:dynamite, :sites)
+    table = sites[site][:routing_table]
+    EEx.eval_file table[:routes]<>"main.eex"
+    :ok
+  end
+
+  def start(_type, _args) do
+    site = Application.fetch_env!(:dynamite, :site)
+    sites = Application.fetch_env!(:dynamite, :sites)
+    port = sites[site][:port]
+    { :ok, _ } = :cowboy.start_http(:http, 
+                                    100,
+                                   [{:port, port}],  
+                                   [{ :env, [{:dispatch, []}]}]
+                                   ) 
+
+    compile_routes
+    device = File.open!("dbg.log", [:write])
+    Application.put_env(:dbg, :device, device) 
+    PelaresCom.Supervisor.start_link([])
+  end
+
+end
